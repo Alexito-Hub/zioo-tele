95dfda20e99f4d905b4ea5b65fd5425466ea9f94
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2024-01-20 18:33:17 -0500

    update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,18 +3,21 @@
 module.exports = {
   name: "Ping",
   description: "Muestra el tiempo de respuesta en milisegundos",
-  commands: ["ping", "ms"],
+  commands: ["ping"],
   async execute(bot, chatId, messageId) {
     const startTime = new Date();
     
     try {
-      await bot.sendMessage(chatId, 'Calculando el tiempo de respuesta...');
+      const responseMessage = await bot.sendMessage(chatId, 'Calculando el tiempo de respuesta...');
+      const endTime = new Date();
+      const responseTime = endTime - startTime;
+
+      await bot.editMessageText(`El tiempo de respuesta es de ${responseTime} milisegundos.`, {
+        chat_id: chatId,
+        message_id: responseMessage.message_id
+      });
     } catch (e) {
       throw e;
     }
-
-    const endTime = new Date();
-    const responseTime = endTime - startTime;
-    bot.editMessageText(`El tiempo de respuesta es de ${responseTime} milisegundos.`, { chat_id: chatId, message_id: messageId });
   }
 };
