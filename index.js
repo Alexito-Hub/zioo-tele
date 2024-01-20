920b13308576ec5a1eddbc0222fc199643bea481
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2024-01-20 03:54:54 -0500

    update index.js

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
-require('dotenv').config()
+require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 
-// Obtén el token desde la variable de entorno
 const botToken = process.env.BOT_TOKEN;
 
 if (!botToken) {
-  console.error('No se proporcionó el token del bot. Asegúrate de establecer la variable de entorno BOT_TOKEN.');
+  console.log('No se proporcionó el token del bot. Asegúrate de establecer la variable de entorno BOT_TOKEN.');
   process.exit(1);
 }
 
 const bot = new TelegramBot(botToken, { polling: true });
 
-// Manejar comandos
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, '¡Hola! Soy tu bot de Telegram.');
+  const keyboard = {
+    resize_keyboard: true,
+    keyboard: [
+      ['Menu', 'Configuración'],
+    ],
+  };
+
+  const options = {
+    reply_markup: JSON.stringify(keyboard),
+  };
+
+  bot.sendMessage(chatId, '¡Hola! Soy tu bot de Telegram.', options);
 });
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Recibí tu mensaje: ' + msg.text);
+  const messageText = msg.text.toLowerCase();
+
+  if (messageText === 'menu') {
+    // Lógica para manejar el botón de menú
+    bot.sendMessage(chatId, 'Seleccionaste el botón de Menú.');
+  } else if (messageText === 'configuración') {
+    // Lógica para manejar el botón de configuración
+    bot.sendMessage(chatId, 'Seleccionaste el botón de Configuración.');
+  } else {
+    bot.sendMessage(chatId, 'Recibí tu mensaje: ' + msg.text);
+  }
 });
