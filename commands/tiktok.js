de81c20bd08d48db02ba88ccf0f9814164f7e757
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2024-01-21 01:08:20 -0500

    tiktok

---
+++ commands/tiktok.js
@@ -0,0 +1,70 @@
+const { fetchJson } = require('../../lib/utils');
+
+module.exports = {
+    name: 'tiktok',
+    description: 'Descarga videos e imágenes de TikTok',
+    commands: ['tiktok', 'tt'],
+
+    async execute(bot, msg, args) {
+        try {
+            if (!args[0]) {
+                await bot.sendMessage(msg.chat.id, '*tiktok <url>*', { reply_to_message_id: msg.message_id });
+                return;
+            }
+
+            await bot.sendMessage(msg.chat.id, '🕛', { reply_to_message_id: msg.message_id });
+            const isAudio = args.includes('--audio') || args.includes('-a');
+
+            const tiktokUrl = args[0];
+            const response = await fetchJson(`https://iam-zio.replit.app/api/tiktok?key=zio&url=${tiktokUrl}`);
+
+            function roundTime(time) {
+                return Math.round(time);
+            }
+
+            const responseMs = Date.now();
+            const responseTime = roundTime(responseMs - msg.date * 1000);
+            const messageRoundTime = (responseTime / 1000).toFixed(3);
+
+            if (response && response.result) {
+                const result = response.result;
+                if (isAudio) {
+                    await bot.sendAudio(msg.chat.id, result.music.url, {
+                        caption: `᳃ ¡Listo! - *🧃 ${messageRoundTime} ms*`,
+                        reply_to_message_id: msg.message_id,
+                    });
+                } else if (result.type === 'video') {
+                    await bot.sendVideo(msg.chat.id, result.video.noWatermark, {
+                        caption: `ㅤ *⋯⋯ TIK TOK ⋯⋯*
+ ∘ *Autor:* ${result.author.name}
+ ∘ *Likes:* ${result.information.likeCount}
+ ∘ *Comentarios:* ${result.information.commentCount}
+ ∘ *Duración:* ${result.video.durationFormatted}
+ ∘ *Fecha:* ${result.information.created_at}
+ ∘ *Titulo:* ${result.information.title}`,
+                        reply_to_message_id: msg.message_id,
+                    });
+                } else if (result.type === 'images') {
+                    for (const image of result.images) {
+                        await bot.sendPhoto(msg.chat.id, image.url.url, {
+                            caption: `᳃ ¡Listo! - *🧃 ${messageRoundTime} ms*`,
+                            reply_to_message_id: msg.message_id,
+                        });
+                    }
+                    await bot.sendAudio(msg.chat.id, result.music.url, {
+                        caption: `᳃ ¡Listo! - *🧃 ${messageRoundTime} ms*`,
+                        reply_to_message_id: msg.message_id,
+                    });
+                }
+                await bot.sendMessage(msg.chat.id, '✅', { reply_to_message_id: msg.message_id });
+            } else {
+                console.log('Error al obtener información');
+                await bot.sendMessage(msg.chat.id, '❎', { reply_to_message_id: msg.message_id });
+                await bot.sendMessage(msg.chat.id, 'No se pudo descargar', { reply_to_message_id: msg.message_id });
+            }
+        } catch (e) {
+            console.error(e);
+            await bot.sendMessage(msg.chat.id, `${e}`, { reply_to_message_id: msg.message_id });
+        }
+    },
+};
--- index.js
+++ index.js
@@ -1,56 +1,65 @@
-const fs = require('fs');
-const path = require('path');
-const TelegramBot = require('node-telegram-bot-api');
-require('./config');
+require('./config')
 require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
+const fs = require('fs')
+const path = require('path')
+const commands = [];
 
 const botToken = process.env.BOT_TOKEN;
 
 if (!botToken) {
-    console.error('No se proporcionó el token del bot. Asegúrate de establecer la variable de entorno BOT_TOKEN.');
+    console.log('No se proporcionó el token del bot. Asegúrate de establecer la variable de entorno BOT_TOKEN.');
     process.exit(1);
 }
 
 const bot = new TelegramBot(botToken, { polling: true });
-const commands = loadCommands();
 
 function loadCommands() {
     const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
-    return commandFiles.map(file => require(path.join(__dirname, 'commands', file)));
-}
-
-function processTextMessage(msg) {
-    const prefixes = global.prefix || ['/'];
-    const isCmd = msg.text && prefixes.some(prefix => msg.text.toLowerCase().startsWith(prefix.toLowerCase()));
-    const command = isCmd
-        ? msg.text.split(' ')[0].slice(prefixes.find(prefix => msg.text.toLowerCase().startsWith(prefix.toLowerCase())).length).toLowerCase()
-        : msg.text.trim().split(' ')[0].toLowerCase();
     
-    const { chat, message_id } = msg;
-    const commandInfo = commands.find(cmd => cmd.commands.includes(command));
-    
-    if (commandInfo) {
-        commandInfo.execute(bot, chat.id, message_id);
+    for (const file of commandFiles) {
+        const command = require(path.join(__dirname, 'commands', file));
+        commands.push(command);
     }
 }
 
-bot.onText(/(.+)/, processTextMessage);
+loadCommands();
 
-bot.on('callback_query', (callbackQuery) => {
-    const { message, data } = callbackQuery;
-    const { chat } = message;
 
-    switch (data) {
-        case 'Menu':
-            bot.editMessageText('No se agregaron comandos a la lista de Menú', { chat_id: chat.id, message_id: message.message_id });
-            break;
-        case 'Juegos':
-            bot.editMessageText('Los juegos no están disponibles por el momento', { chat_id: chat.id, message_id: message.message_id });
-            break;
+bot.onText(/(.+)/, (msg) => {
+    const prefixes = global.prefix || ['/'];
+    const isCmd = msg.text && prefixes.some(prefix => msg.text.toLowerCase().startsWith(prefix.toLowerCase()));
+    const command = isCmd
+      ? msg.text.split(' ')[0].slice(prefixes.find(prefix => msg.text.toLowerCase().startsWith(prefix.toLowerCase())).length).toLowerCase()
+      : msg.text.trim().split(' ')[0].toLowerCase();
+    
+    const chatId = msg.chat.id;
+    const messageId = msg.message_id
+    
+    const commandInfo = commands.find(cmd => cmd.commands.includes(command));
+    
+    if (commandInfo) {
+        commandInfo.execute(bot, chatId, messageId);
+        
     }
 });
 
 
+
+bot.on('callback_query', (callbackQuery) => {
+    const chatId = callbackQuery.message.chat.id;
+    const messageId = callbackQuery.message.message_id;
+    const data = callbackQuery.data;
+
+    switch (data) {
+        case 'Menu':
+            bot.editMessageText('No se agregaron comandos a la lista de Menu', { chat_id: chatId, message_id: messageId });
+            break;
+        case 'Juegos':
+            bot.editMessageText('Los juegos no están disponibles por el momento', { chat_id: chatId, message_id: messageId });
+            break;
+    }
+});
 
 bot.on('new_chat_members', (msg) => {
     const chatId = msg.chat.id;
